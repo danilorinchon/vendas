1f5c580cdc95213e9361007ef7b235d6f02688df
danilo <danilo@danilo-ThinkPad-E470>
2024-04-21 23:19:05 -0300

    primeiro commit

---
+++ src/main/java/com/spc/projetovendas/ProjetoVendas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.spc.projetovendas;
+
+/**
+ *
+ * @author danilo
+ */
+public class ProjetoVendas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
