f5e1ba257db7644953141edbf616960d37c7a27d
Danilo Silva <ds2905@outlook.com>
2024-05-18 06:27:23 -0300

    inclusão dos metodos alterar e excluir na class ClientesDAO

--- src/main/java/br/com/ProjetoVendas/DAO/ClientesDAO.java
+++ src/main/java/br/com/ProjetoVendas/DAO/ClientesDAO.java
@@ -66,12 +66,67 @@ public class ClientesDAO {
     }
 
     //metodo alterar cliente
-    public void alterarCliente() {
+    public void alterarCliente(Clientes obj) {
 
+                   try {
+            // 1 passo criar o comando sql
+            String sql = "update `tb_clientes` set  `nome`=?, `rg`=?, `cpf`=?, `email`=?,"
+                    +"`telefone`=?, `celular`=?, `cep`=?, `endereco`=?, `numero`=?,"
+                    +"`complemento`=?, `bairro`=?, `cidade`=?, `estado`=? where `id`=?)";
+                    + "VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+
+            // 2 passo conectar no DB e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            //stmt.setInt(1, NULL);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getTelefone());
+            stmt.setString(6, obj.getCelular());
+            stmt.setString(7, obj.getCep());
+            stmt.setString(8, obj.getEndereco());
+            stmt.setInt(9, obj.getNumero());
+            stmt.setString(10, obj.getComplemento());
+            stmt.setString(11, obj.getBairro());
+            stmt.setString(12, obj.getCidade());
+            stmt.setString(13, obj.getEstado());
+            stmt.setInt(14, obj.getId());
+
+            // 3 passo executar o comando sql
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Atualizado com sucesso");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Ops! A atualização do cliente falhou. Erro: " + erro);
+
+        }
     }
 
     //metodo excluir cliente
-    public void excluirCliente() {
+    public void excluirCliente(Clientes obj) {
+
+        try {
+            // 1 passo criar o comando sql
+            String sql = "delete from `tb_clientes` where `id` = ?;";
+
+            // 2 passo conectar no DB e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            //stmt.setInt(1, NULL);
+            stmt.setInt(1, obj.getId());
+
+            // 3 passo executar o comando sql
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Ops! A exclusão do cliente falhou. Erro: " + erro);
+
+        }
 
     }
 
